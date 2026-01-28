0138551cf444bdc1973668228b5702ddd3c993e0
pawan-pathak12 <214448034+pawan-pathak12@users.noreply.github.com>
2026-01-28 19:01:01 +0545

    Intial Setup done added required packages , registration to prgram.cs

---
+++ src/UserAuth.Api/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UserAuth.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+    }
+}
