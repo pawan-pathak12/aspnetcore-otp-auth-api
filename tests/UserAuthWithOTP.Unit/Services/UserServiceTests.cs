af5a538a49ecea38f8dc2ab7c4f4dcd6d8291db1
pawan-pathak12 <214448034+pawan-pathak12@users.noreply.github.com>
2026-03-07 20:09:20 +0545

    test(userservice): Add passig unit test of UserService

---
+++ tests/UserAuthWithOTP.Unit/Services/UserServiceTests.cs
@@ -0,0 +1,157 @@
+﻿using UserAuth.Api.Data;
+using UserAuth.Api.Entities;
+using UserAuth.Api.Interfaces.Service;
+using UserAuth.Api.Repository.InMemory;
+using UserAuth.Api.Services;
+
+namespace UserAuthWithOTP.Unit.Services
+{
+    [TestClass]
+    public class UserServiceTests
+    {
+        private IUserService _userService = null!;
+        private InMemoryUserRepo _userRepo = null!;
+
+        private string Password = "HelloUser!";
+        [TestInitialize]
+        public void TestInit()
+        {
+            var dbContext = new InMemoryDbContext();
+            _userRepo = new InMemoryUserRepo(dbContext);
+
+            _userService = new UserService(_userRepo);
+        }
+
+        #region Positive Path
+
+        [TestMethod]
+        public async Task AddAsync_WhenValid_ReturnTrue()
+        {
+            //Arrange
+            var user = ReturnUser();
+
+            //Act
+            var result = await _userService.CreateAsync(user);
+
+            //Assert
+            Assert.IsTrue(result.IsSuccess);
+        }
+
+        [TestMethod]
+        public async Task GetByIdAsync_WhenUserExists_ReturnUser()
+        {
+            //Arrange
+            var user = await CreateAndReturnUser();
+
+            //Act
+
+            var result = await _userService.GetByIdAsync(user.Id);
+            //Assert
+            Assert.IsNotNull(result);
+
+        }
+
+        [TestMethod]
+        public async Task GetAllAsync_WhenUsersExist_ReturnUsers()
+        {
+            //Arrange
+            var user = await CreateAndReturnUser();
+
+            //Act
+            var result = await _userService.GetAllAsync();
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public async Task UpdateAsync_WhenUserExists_ReturnTrue()
+        {
+            //Arrange
+            var user = await CreateAndReturnUser();
+
+            var update = new User
+            {
+                Id = user.Id,
+                Email = user.Email,
+                IsActive = true,
+                IsVerified = true,
+                Role = "Admin",
+                Password = user.Password
+            };
+
+            //Act
+            var result = await _userService.UpdateAsync(update);
+
+            //Assert
+            Assert.IsTrue(result);
+            var userData = await _userRepo.GetByIdAsync(user.Id);
+            Assert.IsNotNull(userData);
+            Assert.AreEqual(update.Role, userData.Role);
+        }
+
+        [TestMethod]
+        public async Task DeleteAsync_WhenUserExist_ReturnTrue()
+        {
+            //Arrange
+            var user = await CreateAndReturnUser();
+
+            //Act
+            var result = await _userService.DeleteAsync(user.Id);
+
+            //Assert
+            Assert.IsTrue(result);
+
+        }
+
+        [TestMethod]
+        public async Task GetByEmailAsync_WhenUserExistsOfEnterEmail_ReturnUser()
+        {
+            //Arrange
+            var user = await CreateAndReturnUser();
+
+            //Act
+            var result = await _userService.GetByEmailAsync(user.Email);
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(user.Password, result.Password);
+        }
+
+        #endregion
+
+
+        #region Negative Path
+
+        #endregion
+
+        private async Task<User?> CreateAndReturnUser()
+        {
+            var rand = new Random();
+            var user = new User
+            {
+                Email = $"testuser{rand.Next(1000, 9999)}@gmail.com",
+                Password = Password,
+                IsActive = true,
+                Role = "User",
+                IsVerified = true
+            };
+
+            var userId = await _userRepo.AddAsync(user);
+            return await _userRepo.GetByIdAsync(userId);
+        }
+        private User ReturnUser()
+        {
+            var rand = new Random();
+            return new User
+            {
+                Email = $"testuser{rand.Next(1000, 9999)}@gmail.com",
+                Password = Password,
+                IsActive = true,
+                Role = "User",
+                IsVerified = true
+            };
+        }
+    }
+
+}
