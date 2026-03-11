f17faa017313d946c34c1dbe4cdaf2bf701311c3
pawan-pathak12 <214448034+pawan-pathak12@users.noreply.github.com>
2026-03-11 21:46:00 +0545

    test(usercontroller): Added test method logic of userController ; still test is falling

--- tests/UserAuthWithOTP.API/Controller/UserControllerTest.cs
+++ tests/UserAuthWithOTP.API/Controller/UserControllerTest.cs
@@ -1,112 +1,179 @@
-﻿using UserAuthWithOTP.API.Fixtures;
+﻿using System.Net;
+using System.Net.Http.Json;
+using UserAuth.Api.Entities;
+using UserAuthWithOTP.API.Fixtures;
 
 namespace UserAuthWithOTP.API.Controller
 {
     [TestClass]
     public class UserControllerTest : IntegrationTestBase
     {
-        //Authorizattion is used so sent token  to pass 
+        private string Password = "HelloTesting!1";
+
+        //Authorizattion is used so sent token to pass
 
         #region Postive Path
 
         [TestMethod]
-        public async Task CreateUser_When_Return()
+        public async Task CreateUser_WhenValid_Return201()
         {
-            //Arrange 
+            //Arrange
+            var request = new User
+            {
+                Email = "testuser11111@gmail.com",
+                CreateAt = DateTime.UtcNow,
+                Role = "User",
+                IsActive = true,
+                IsVerified = true,
+                Password = Password
+            };
 
-            //Act 
+            //Act
+            var resposne = await _client.PostAsJsonAsync("/api/User", request);
 
             //Assert
+            Assert.AreEqual(HttpStatusCode.Created, resposne.StatusCode);
         }
 
         [TestMethod]
-        public async Task GetUserById_When_Return()
+        public async Task GetUserById_WhenUserExists_Return200()
         {
-            //Arrange 
+            //Arrange
+            var user = await testDataBuilder.CreateAndReturnUser();
 
-            //Act 
+            //Act
+            var response = await _client.GetAsync($"/api/User/{user.Id}");
 
             //Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
         }
 
         [TestMethod]
-        public async Task GetAllUsers_When_Return()
+        public async Task GetAllUsers_WhenUserExists_Return200()
         {
-            //Arrange 
+            //Arrange
+            await testDataBuilder.CreateAndReturnUser();
 
-            //Act 
+            //Act
+            var response = await _client.GetAsync("/api/User");
 
             //Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
         }
 
         [TestMethod]
-        public async Task UpdateUser_When_Return()
+        public async Task UpdateUser_WhenValid_Return200()
         {
-            //Arrange 
+            //Arrange
+            var user = await testDataBuilder.CreateAndReturnUser();
 
-            //Act 
+            var request = new User
+            {
+                Id = user.Id,
+                Email = "updateduser@gmail.com",
+                CreateAt = user.CreateAt,
+                Role = "Admin",
+                IsActive = true,
+                IsVerified = true,
+                Password = Password
+            };
+
+            //Act
+            var response = await _client.PutAsJsonAsync($"/api/User/{user.Id}", request);
 
             //Assert
-
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
         }
 
         [TestMethod]
-        public async Task DeleteUser_When_Return()
+        public async Task DeleteUser_WhenValid_Return200()
         {
-            //Arrange 
+            //Arrange
+            var user = await testDataBuilder.CreateAndReturnUser();
 
-            //Act 
+            //Act
+            var response = await _client.DeleteAsync($"/api/User/{user.Id}");
 
             //Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
         }
 
         #endregion
+
 
         #region Negative Path
 
         [TestMethod]
         public async Task CreateUser_WhenInvalid_Return400()
         {
-            //Arrange 
+            //Arrange
+            var request = new User
+            {
+                Email = "", //invalid email
+                CreateAt = DateTime.UtcNow,
+                Role = "User",
+                IsActive = true,
+                IsVerified = true,
+                Password = Password
+            };
 
-            //Act 
+            //Act
+            var resposne = await _client.PostAsJsonAsync("/api/User", request);
 
             //Assert
+            Assert.AreEqual(HttpStatusCode.BadRequest, resposne.StatusCode);
         }
 
         [TestMethod]
-        public async Task GetUserById_WhenUserNotFound_Return400()
+        public async Task GetUserById_WhenUserNotFound_Return404()
         {
-            //Arrange 
+            //Arrange
+            var invalidId = Guid.NewGuid();
 
-            //Act 
+            //Act
+            var response = await _client.GetAsync($"/api/User/{invalidId}");
 
             //Assert
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
         }
-
 
         [TestMethod]
         public async Task UpdateUser_WhenInvalid_Return400()
         {
-            //Arrange 
+            //Arrange
+            var user = await testDataBuilder.CreateAndReturnUser();
 
-            //Act 
+            var request = new User
+            {
+                Id = user.Id,
+                Email = "", //invalid
+                CreateAt = user.CreateAt,
+                Role = "User",
+                IsActive = true,
+                IsVerified = true,
+                Password = Password
+            };
+
+            //Act
+            var response = await _client.PutAsJsonAsync($"/api/User/{user.Id}", request);
 
             //Assert
-
+            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
         }
 
         [TestMethod]
-        public async Task DeleteUser_WhenUserNotFound_Return400()
+        public async Task DeleteUser_WhenUserNotFound_Return404()
         {
-            //Arrange 
+            //Arrange
+            var invalidId = Guid.NewGuid();
 
-            //Act 
+            //Act
+            var response = await _client.DeleteAsync($"/api/User/{invalidId}");
 
             //Assert
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
         }
 
         #endregion
-
     }
 }
